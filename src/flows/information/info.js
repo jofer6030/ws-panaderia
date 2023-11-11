8c8d4a18f3a15a2dc49a6d51fda4c9c9236b686c
Jose Fernando <jofer0628@gmail.com>
2023-11-10 21:05:36 -0500

    information context changed

--- src/flows/information/info.js
+++ src/flows/information/info.js
@@ -1,62 +1,55 @@
 export const contentCompany = `
-Bienvenidos a "Pan Artesanal Memo". Nos especializamos en la elaboración de panes rústicos y deliciosos, hechos con ingredientes naturales y sin aditivos. Todos nuestros productos se hornean diariamente en nuestro horno tradicional para garantizar la frescura y calidad. Estamos ubicados en el centro de la ciudad, en la calle Principal #123. También ofrecemos servicio de entrega a domicilio dentro de un radio de 10 km. Nuestro horario de atención es de lunes a sábado, de 7:00 a.m. a 7:00 p.m.
+Bienvenidos a "Pan Artesanal Memo". Nos especializamos en la elaboración de panes rústicos y deliciosos, hechos con ingredientes naturales y sin aditivos. Todos nuestros productos se hornean diariamente en nuestro horno tradicional para garantizar la frescura y calidad. Nuestro horario de atención es de lunes a sábado, de 7:00 a.m. a 7:00 p.m. Somos una panadería que usa masa madre y pre fermento, nuestros productos no llevan preservantes, trabajamos bajo a pedido. No manejamos stock, y nuestro servicio al cliente siempre busca tener una mejor satisfacción con nuestros productos y cumplimiento de las fechas de entrega
 
 Tipos panes para hamburguesa:
-Siguientes productos pan brioche, pan de papa, pan brioche premium y pan brioche de colores.
+-Siguientes productos pan brioche, pan brioche premium y pan brioche de colores.
+
 Características:
--Tiene un diámetro de 11 cm y un alto de entre 5 a 6 cm, tiene un peso de 90 gramos.
+-Tienen un diámetro de 11 cm y un alto de entre 5 a 6 cm, tiene un peso de 90 gramos.
+
+Personalización:
 -Puede ir acompañados de ajonjolí blanco, negro o sin ajonjolí.
--El pan tiene un tiempo de duración de 5 días en medio ambiente y refrigerador de 12 días.
+-Los panes de colores tenemos rojo, negro y verde.
+
+Tiempo de vida o duración del Pan:
+-El pan tiene un tiempo de duración de 5 días en medio ambiente y refrigerador de 12 días
+
 Diferencia entre tipos de panes:
--Pan de papa lleva papa amarilla como ingrediente
--Pan tipo brioche vs brioche premium esté lleva mantequilla y 100% leche.
-Los precios son los siguientes: 
+Pan tipo brioche vs brioche premium esté lleva mantequilla y 100% leche.
+
+Precio: 
 -Pan brioche S/ 1.20 la unidad.
--Pan de papa S/ 1.40 la unidad.
 -Pan brioche premium S/ 1.80 la unidad.
--Pan brioche de color S/ 1.30 la unidad.
-Reglas:
--No ofrecemos muestras.
--Pedidos pertmitidos desde 12 unidades a más.
--Para confirmar un pedido debe abonar el 50% del pedido y cancelar el 50% el mismo día de la entrega.
--Solo manéjanos únicos precios, no hay precio por mayor.
--Se recepciona pedidos con dos días de anticipación.
--Solo emitimos boletas electrónicas, solicitar el ruc o dni y correo.
-
-Tipos de panes rellenos
-Siguientes productos pan con queso, pan mixto (queso, jamón y orégano) , pan con aceituna, pan con cabanossi (queso y cabanossi).
-Los precios son los siguientes: 
--Pan de queso S/ 3.00 la unidad.
--Pan aceituna S/ 2.50 la unidad.
--Pan mixto S/ 3.50 la unidad.
--Pan cabanossi S/ 3.50 la unidad.
-Reglas:
--No ofrecemos muestras.
--Pedidos pertmitidos desde 6 unidades a más..
--Para confirmar un pedido debe abonar el 50% del pedido y cancelar el 50% el mismo día de la entrega.
--Solo manéjanos únicos precios, no hay precio por mayor.
--Se decepciona pedidos con dos días de anticipación.
--Solo emitimos boletas electrónicas, solicitar el ruc o dni y correo.
-
-Tipos panes de molde:
-Siguientes productos pan integral, pan integral multigranos, pan de quinua, pan de linaza.
-Características
--Tiene un peso de 600 gramos.
--El pan tiene un tiempo de duración de 4 días en medio ambiente y refrigerador de 7 días.
--Todos los panes de moldes son elaborados con masa madre.
-Los precios son los siguientes: 
--Pan integral S/ 10.00 la unidad.
--Pan integral multigranos S/ 10.00 la unidad.
--Pan quinua S/ 10.00 la unidad.
--Pan linaza S/ 10.00 la unidad.
-Reglas:
--No ofrecemos muestras.
--Pedidos pertmitidos desde 2 unidades a más.
--Para confirmar un pedido debe abonar el 50% del pedido y cancelar el 50% el mismo día de la entrega.
--Solo manéjanos únicos precios, no hay precio por mayor.
--Se decepciona pedidos con dos días de anticipación.
--Solo emitimos boletas electrónicas, solicitar el ruc o dni y correo.
+-Pan brioche de color S/ 1.50 la unidad.
 
 Medio de Pago:
 Yape, número 922545942 a nombre de Alfonso Tafur.
-`
+Comprobante de pago:
+Solo emitimos boletas electrónicas, solicitar el ruc o dni y el correo.
+
+Panetón:
+-El panetón artesanal
+
+Características:
+-Lleva Germen de Trigo, Lecitina de Soya, Pecanas, Higo y Naranja Confitada. 
+-Tiene un peso de 700 gramos.
+
+Tiempo de vida o duración del Pan:
+-El pan tiene un tiempo de duración de 30 días en medio ambiente.
+
+Precio: 
+-Precio regular S/ 26.00
+-Precio preventa S/ 23.00
+-Precio por caja de 6 unidades S/ 132.00
+
+Reglas:
+-No ofrecemos muestras de panetones.
+-Pedido mínimo es de 1 unidades.
+-Para confirmar un pedido debe abonar el 50% del pedido y cancelar el 50% el mismo día de la entrega.
+-Se recepción pedidos con tres días de anticipación.
+
+Medio de Pago:
+Yape, número 922545942 a nombre de Alfonso Tafur.
+Comprobante de pago:
+Solo emitimos boletas electrónicas, solicitar el ruc o dni y el correo.
+`;
