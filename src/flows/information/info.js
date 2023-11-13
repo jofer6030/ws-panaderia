a02c1ea2ed641b6dce045c985a18122983856a2a
Jose Fernando <jofer0628@gmail.com>
2023-11-13 13:40:05 -0500

    info updated v3

--- src/flows/information/info.js
+++ src/flows/information/info.js
@@ -1,55 +1,67 @@
-export const contentCompany = `
-Bienvenidos a "Pan Artesanal Memo". Nos especializamos en la elaboración de panes rústicos y deliciosos, hechos con ingredientes naturales y sin aditivos. Todos nuestros productos se hornean diariamente en nuestro horno tradicional para garantizar la frescura y calidad. Nuestro horario de atención es de lunes a sábado, de 7:00 a.m. a 7:00 p.m. Somos una panadería que usa masa madre y pre fermento, nuestros productos no llevan preservantes, trabajamos bajo a pedido. No manejamos stock, y nuestro servicio al cliente siempre busca tener una mejor satisfacción con nuestros productos y cumplimiento de las fechas de entrega
+export const contentCompany = `Detalle de la Panadería
+Nombre: Pan Artesanal Memo.
+Detalle: Somos una tienda virtual, solo atendemos pedidos.
+Ubicación: Av. Valentín Espejo 227 San Juan de Miraflores altura del mercado valle Sharon.
 
-Tipos panes para hamburguesa:
--Siguientes productos pan brioche, pan brioche premium y pan brioche de colores.
+Productos y Precios
+Pan para hamburguesa: Pan brioche, Pan Brioche de colores y Pan Brioche Premium.
+Panetones: Panetón Artesanal.
 
-Características:
--Tienen un diámetro de 11 cm y un alto de entre 5 a 6 cm, tiene un peso de 90 gramos.
+Descripción del Producto:
+Pan Brioche: nuestro pan tiene un diámetro de 11 cm de ancho y entre 5 a 6 cm de altura, con un peso de 90 gramos 
+Pan Brioche de colores: nuestro pan tiene un diámetro de 11 cm de ancho y entre 5 a 6 cm de altura, con un peso de 90 gramos y los colores disponibles son negro, rojo, verde y naranja.
+Pan Brioche Premium:   nuestro pan tiene un diámetro de 11 cm de ancho y entre 4 a 5 cm de altura, con un peso de 80 gramos, lleva mantequilla y 100% de leche
+Panetón Artesanal:  nuestro panetón artesanal se diferencia porque lleva germen de Trigo, Lecitina de Soya, Pecanas, Higo, Naranja Confitada y tiene un peso de 700 gramos
 
-Personalización:
--Puede ir acompañados de ajonjolí blanco, negro o sin ajonjolí.
--Los panes de colores tenemos rojo, negro y verde.
+Precios:
+Pan brioche tiene un precio de S/ 1.20 la unidad
+Pan brioche de colores tiene un precio de S/ 1.50 la unidad
+Pan brioche premium tiene un precio de S/ 1.80 la unidad
+Panetón Artesanal tiene un precio de S/ 26.00 la unidad
 
-Tiempo de vida o duración del Pan:
--El pan tiene un tiempo de duración de 5 días en medio ambiente y refrigerador de 12 días
+Promociones:
+Los Panetones para el mes de noviembre tenemos el precio de pre venta a S/ 23.00 y por pedidos a mayor de 6 unidades el precio es de S/22.00
 
-Diferencia entre tipos de panes:
-Pan tipo brioche vs brioche premium esté lleva mantequilla y 100% leche.
+Opciones de Personalización:
+Los panes de hamburguesa se puede personalizar el ajonjolí, pude ir de color blanco, negro o sin ajonjolí.
 
-Precio: 
--Pan brioche S/ 1.20 la unidad.
--Pan brioche premium S/ 1.80 la unidad.
--Pan brioche de color S/ 1.50 la unidad.
+Información sobre la Frescura y Duración:
+Los panes para hamburguesa: tienen un tiempo de duración de 5 a 14 días al medio ambiente.
+Los panetones: tiene una duración de 30 días al medio ambiente
 
-Medio de Pago:
-Yape, número 922545942 a nombre de Alfonso Tafur.
-Comprobante de pago:
-Solo emitimos boletas electrónicas, solicitar el ruc o dni y el correo.
+Imágenes de los Productos:
+Foto Panetón Artesanal : https://photos.app.goo.gl/MVj1NVMFbt4BFyREA
+Foto Pan Brioche: https://photos.app.goo.gl/Assh5FELuM1qH3Gh6
+Foto Pan Brioche Premium: https://photos.app.goo.gl/MWHK3pGrUUKEMP7X6
+Foto Pan Brioche de Colores: https://photos.app.goo.gl/2zwc21qUezApJKtC9
 
-Panetón:
--El panetón artesanal
+Disponibilidad: 
+Los panes de hamburguesa se elaboran a pedido, el cual se debe realizar con dos días de anticipación.
+Los panetones solo disponible en los meses de noviembre y diciembre. Para cantidades mayores a 30 unidades se debe realizar con 3 días de anticipación. 
 
-Características:
--Lleva Germen de Trigo, Lecitina de Soya, Pecanas, Higo y Naranja Confitada. 
--Tiene un peso de 700 gramos.
+Operaciones de la Panadería
+Horarios de Funcionamiento: Atendemos de lunes a sábado de 9:00 am a 6pm.
 
-Tiempo de vida o duración del Pan:
--El pan tiene un tiempo de duración de 30 días en medio ambiente.
+Políticas de Pedido y Entrega:
+Pedido:
+Los panes de hamburguesa: el pedido se debe realizar con dos días de anticipación.
+Los panetones: solo disponible en los meses de noviembre y diciembre. Para pedidos a mayores de 10 unidades se debe realizar con tres días de anticipación. 
 
-Precio: 
--Precio regular S/ 26.00
--Precio preventa S/ 23.00
--Precio por caja de 6 unidades S/ 132.00
+Entrega:  Puedo recoger su pedido en nuestro local o lo podemos enviar a su domicilio.
 
-Reglas:
--No ofrecemos muestras de panetones.
--Pedido mínimo es de 1 unidades.
--Para confirmar un pedido debe abonar el 50% del pedido y cancelar el 50% el mismo día de la entrega.
--Se recepción pedidos con tres días de anticipación.
+Métodos de Pago Aceptados:
+Panes de hamburguesa: para confirma el pedido se debe cancelar el 50% del precio total del pedido y el resto el día de la entrega.
+Panetones: se cancela la totalidad del precio del pedido.
 
-Medio de Pago:
-Yape, número 922545942 a nombre de Alfonso Tafur.
-Comprobante de pago:
-Solo emitimos boletas electrónicas, solicitar el ruc o dni y el correo.
+Procedimiento para Pedidos Especiales:
+Panetones: para pedidos de más de 10 panetones debe cancelar el 50% del precio total del pedido y el resto el día de la entrega.
+
+Comunicación y Servicio al Cliente
+Política de Cancelación y Devolución:
+Una vez pasado 24 horas de solicitado el pedido no hay devolución del dinero.
+Se realizará devolución del dinero una vez se haya confirmado el pago en el mismo día después de 3 horas.
+Se puede cancelar el pedido en el mismo día de la solicitud de pedido.
+
+Delivery:
+Se debe solicitar la dirección para la entrega del pedido y el servicio de delivery el precio está sujeto a la distancia de nuestra ubicación.
 `;
